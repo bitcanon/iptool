b3f1f1cc048abdb1c1277f1d39cd16d438769d5c
Mikael Schultz <bitcanon@proton.me>
2023-12-25 18:36:59 +0100

    Update information

--- utils/output.go
+++ utils/output.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2024 Mikael Schultz <bitcanon@proton.me>
+Copyright © 2024 Mikael Schultz <bitcanon@conf-t.se>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
